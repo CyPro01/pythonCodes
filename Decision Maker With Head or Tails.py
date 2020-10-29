24878b7fe76cfc2d4b1020070fe8d36c151030f4
CyPro01 <73633178+CyPro01@users.noreply.github.com>
2020-10-29 12:23:43 +0530

    Add files via upload

---
+++ Decision Maker With Head or Tails.py
@@ -0,0 +1,35 @@
+import random
+import time
+import sys
+
+
+def Coin():
+    print("Flipping Coin")
+    string ="\|/\|/\|/\|/\|/\|/\|/\|/\|/"
+
+    for i in string:
+        time.sleep(.1)
+        print(i)
+
+    a = ['Heads','Tails']
+    b = random.choice(a)
+    return b
+
+input("First Decide! Then press Enter")
+
+print(Coin())
+
+time.sleep(2)
+
+x = input("Want to go for best of 3 ? Press 'Y' for Yes and 'N' for No: ").upper()
+coin = []
+if x == 'Y':
+    for i in range(0,3):
+        face = Coin()
+        coin.append(face)
+    print(coin)
+elif x == 'N':
+    print("OK. Bye!")
+else:
+    print("Incorrect choice!!")
+
